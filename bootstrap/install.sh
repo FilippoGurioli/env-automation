1d03b429bec1d3f26ca72a41e5ae3ce2ac9d69cd
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-08-27 15:34:56 +0200

    fix: install sudo in order to enable sudo for wheel group in bootstrap

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -74,7 +74,7 @@ mkdir -p /mnt/boot
 mount "$EFI_PART" /mnt/boot
 
 info "Installing base functionalities"
-pacstrap -K /mnt base linux-zen linux-firmware grub efibootmgr vim
+pacstrap -K /mnt base linux-zen linux-firmware grub efibootmgr vim sudo
 
 info "Generating file systems table"
 genfstab -U /mnt >> /mnt/etc/fstab
