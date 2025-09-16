5412c147e0e8166e0b968c04f511199dba82d766
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-09-16 21:25:09 +0200

    feat(vm): take variables from .env and redirect output to a file

--- vm-helper/env-automation-run.sh
+++ vm-helper/env-automation-run.sh
@@ -3,11 +3,20 @@
 STATUS=$(systemctl is-active libvirtd)
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+if [ -f "$SCRIPT_DIR/.env" ]; then
+    source "$SCRIPT_DIR/.env"
+else
+    echo "⚠️  No .env file found at $SCRIPT_DIR/.env"
+    exit 1
+fi
+
 if [ "$STATUS" != "active" ]; then
 	sudo systemctl enable --now libvirtd
 fi
 
-"$SCRIPT_DIR/lib/wipe-vm.sh" && "$SCRIPT_DIR/lib/build-vm.sh" && "$SCRIPT_DIR/lib/launch-install.sh" "$@"
+"$SCRIPT_DIR/lib/wipe-vm.sh" && "$SCRIPT_DIR/lib/build-vm.sh" 
+
+"$SCRIPT_DIR/lib/launch-install.sh" "$HOST" "$USER" "$PASSWORD" > "$(date + '%Y-%m-%d_%H-%M-%S').log" 2>&1
 
 virsh shutdown arch
 sleep 5 # waiting to shutdown correctly
