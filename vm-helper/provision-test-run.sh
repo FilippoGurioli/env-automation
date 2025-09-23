3efdd3cb7940c776b46305d257a9399400abfe43
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-09-23 19:12:40 +0200

    refactor: rename env-automation-test into full-test-run

---
+++ vm-helper/provision-test-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#TODO: create a vm that has only done the bootstrap phase using vm, ssh and 123 as inputs.
+#TODO: then add ssh compatibility.
+#TODO: then create an image from that vm.
+#TODO: then here exploit that image to launch the provision phase with interactive ssh log.
