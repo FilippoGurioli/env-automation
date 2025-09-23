5af398a8176cf1a836eda04aa42e83e7fa0fb27c
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-09-23 16:29:32 +0200

    chore: temporarely commented provision scripts to spot a bug

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail # fail fast strategy
 
-# Checking connectivity
 echo "Checking connectivity"
 if ! ping -c 1 -W 2 8.8.8.8 &> /dev/null; then
 	echo "No connection, aborting bootstrap install, re-launch this script once there is connectivity"
@@ -11,7 +10,7 @@ else
 	echo "Connected, starting bootstrap install"
 fi
 
-# Setting variables
+echo "Setting variables..."
 export HOST="$1"
 export USER="$2"
 export PASSWORD="$3"
@@ -22,22 +21,23 @@ else
 	export VM_ENV=0
 fi
 
-# Downloading scripts
+echo "Downloading scripts..."
 curl -fsSL "$BASE_URL/utils.sh" -o /mnt/utils.sh
 curl -fsSL "$BASE_URL/bootstrap/install.sh" -o bootstrap.sh
 curl -fsSL "$BASE_URL/bootstrap/post-install.sh" -o /mnt/post-bootstrap.sh
 curl -fsSL "$BASE_URL/provision/install.sh" -o /mnt/provision.sh
 curl -fsSL "$BASE_URL/provision/post-install.sh" -o /mnt/post-provision.sh
 
-# Sourcing scripts as ISO root
+echo "Sourcing scripts..."
 source /mnt/utils.sh
 source ./bootstrap.sh
 
-# Running commands in system root
+info "Launching arch-chroot scripts..."
 arch-chroot /mnt /bin/bash -c "source /utils.sh && /post-bootstrap.sh"
-arch-chroot /mnt /bin/bash -c "source /utils.sh && /provision.sh"
-arch-chroot /mnt /bin/bash -c "source /utils.sh && /post-provision.sh"
+# arch-chroot /mnt /bin/bash -c "source /utils.sh && /provision.sh"
+# arch-chroot /mnt /bin/bash -c "source /utils.sh && /post-provision.sh"
 
+info "Cleaning up..."
 rm /mnt/utils.sh
 rm /bootstrap.sh
 rm /mnt/post-bootstrap.sh
